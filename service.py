fa1078ba38f07f9bf13d1c1499d37de51300b472
sahsaide <shassane@csss.es>
2026-02-03 02:02:23 +0200

    service.py

--- service.py
+++ service.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 
-# 1. ABSTRACTION
 class Service(ABC):
     def __init__(self, name, price):
         self.name = name
@@ -10,7 +9,6 @@ class Service(ABC):
     def get_details(self):
         pass
 
-# INHERITANCE: Existing Services
 class Manicure(Service):
     def __init__(self):
         super().__init__("Gel Manicure", 45)
@@ -23,7 +21,6 @@ class Pedicure(Service):
     def get_details(self):
         return f"{self.name} - Includes massage and scrub (${self.price})"
 
-# INHERITANCE: New Services
 class AcrylicSet(Service):
     def __init__(self):
         super().__init__("Acrylic Full Set", 85)
